3566640974c8929275020a049241fc1794465b32
Blaeriz <pinglehrishikesh@yahoo.in>
2025-03-01 03:37:10 +0530

    added ability to download resume

---
+++ src/routes/download_resume/+server.ts
@@ -0,0 +1,20 @@
+import { error } from "@sveltejs/kit";
+import fs from "fs";
+import path from "path";
+
+export async function GET() {
+  const filePath = path.resolve("static/Hrishikesh_Pingle_Resume.pdf"); // Adjust path if needed
+
+  if (!fs.existsSync(filePath)) {
+    throw error(404, "File not found");
+  }
+  const pdfBuffer = fs.readFileSync(filePath);
+
+  return new Response(pdfBuffer, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition":
+        'attachment; filename="Hrishikesh_Pingle_Resume.pdf"',
+    },
+  });
+}
