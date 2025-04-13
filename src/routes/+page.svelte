a3948082f8351e5b9560653fdb14f1bdac8f4d4b
Blaeriz <pinglehrishikesh@yahoo.in>
2025-04-14 04:07:32 +0530

    removed bs, revamped Experience section, added a certification section which will be updated soon.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,24 @@
   let contactFormInputStyles = "h-10 w-full rounded-md p-2 text-xl placeholder-black"
   let contactFormMessageInputStyles = "h-28 w-full rounded-md p-2 text-xl placeholder-black resize-none text-start"
 
+  const timeline = [
+    {
+      title: "Software Engineer",
+      date: "2023 - Present",
+      description: "Working on scalable systems at Example Corp."
+    },
+    {
+      title: "Frontend Developer",
+      date: "2021 - 2023",
+      description: "Built interactive UIs with React and Tailwind."
+    },
+    {
+      title: "Intern",
+      date: "2020",
+      description: "Learned how not to break production 😅"
+    }
+  ];
+
   
 
   onMount(() => {
@@ -171,19 +189,175 @@
         <!-- Experience -->
         <div class="pt-5 pb-16">
           <h2 class={titleStyles}>Experience</h2>
-          <p class={subTitleStyles}>Revisiting Noteworthy Ventures: A Recap of Impactful Projects and Achievements</p>
         </div>
-        <!-- Add Experience Here -->
 
+        <section id="experience" class="flex flex-col justify-start w-[90%]" aria-label="Work experience">
+          <div>
+              <ol class="group/list">
+                  <li class="mb-12">
+                      <div class="flex flex-row align-middle justify-center items-center pb-1 transition-all lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                          <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="Jun – Jul 2024">Jun – Jul 2024</header>
+                          <div class="flex flex-col align-middle justify-center items-center flex-[3_6]">
+                              <h3 class="font-medium leading-snug text-slate-200 ">
+                                  <div>
+                                      <a
+                                          class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+                                          href="https://www.onwardgroup.com/"
+                                          target="_blank"
+                                          rel="noreferrer noopener"
+                                          aria-label="Software Engineer, Intern at Onward Technologies"
+                                      >
+                                          <span>
+                                              Software Engineer, Intern ·<!-- -->
+                                              <span class="inline-block">
+                                                  Onward Technologies
+                                                  <svg
+                                                      xmlns="http://www.w3.org/2000/svg"
+                                                      viewBox="0 0 20 20"
+                                                      fill="currentColor"
+                                                      class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                                      aria-hidden="true"
+                                                  >
+                                                      <path
+                                                          fill-rule="evenodd"
+                                                          d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                          clip-rule="evenodd"
+                                                      ></path>
+                                                  </svg>
+                                              </span>
+                                          </span>
+                                      </a>
+                                  </div>
+                              </h3>
+                              <p class="mt-2 text-base leading-normal text-white">
+                                During my internship as a Software Engineering Intern at Bentley Motors, I conducted in-depth Exploratory Data Analysis (EDA) and developed three proof-of-concept (POC) models for the marketing team, which led to a 40% improvement in targeted campaign effectiveness and engagement. I collaborated with the team using Jupyter Notebook and worked extensively with tools, for data cleaning, processing, and visualization. Additionally, I engineered an interactive dashboard using Svelte to visualize cloud expenses from Azure, AWS, and GCP APIs. This dashboard featured real-time tracking and integrated a forecasting system that predicted monthly costs with an 85% accuracy rate.
+                              </p>
+                              <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Jupyter Notebook</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Pandas</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">NumPy</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">SciPy</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Matplotlib</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Seaborn</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Scikit-learn</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Statsmodels</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Plotly</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Svelte</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Azure</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">AWS</div>
+                                </li>
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">GCP</div>
+                                </li>                                
+                                <li class="mr-1.5 mt-2">
+                                  <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Salesforce</div>
+                                </li>
+                              </ul>
+                          </div>
+                      </div>
+                  </li>
+                  <li class="mb-12">
+                    <div class="flex flex-row align-middle justify-center items-center pb-1 transition-all lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                        <header class="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="Jun – Jul 2024">Dec - Jan 2024</header>
+                        <div class="flex flex-col align-middle justify-center items-center flex-[3_6]">
+                            <h3 class="font-medium leading-snug text-slate-200 ">
+                                <div>
+                                    <a
+                                        class="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base"
+                                        href="https://winspectindia.com"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                        aria-label="Software Engineer, Intern at WinspectTechnologies Pvt. Ltd."
+                                    >
+                                        <span>
+                                            Software Engineer, Intern ·<!-- -->
+                                            <span class="inline-block">
+                                                Winspect Technologies Pvt. Ltd.
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                    class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                                    aria-hidden="true"
+                                                >
+                                                    <path
+                                                        fill-rule="evenodd"
+                                                        d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                        clip-rule="evenodd"
+                                                    ></path>
+                                                </svg>
+                                            </span>
+                                        </span>
+                                    </a>
+                                </div>
+                            </h3>
+                            <p class="mt-2 text-base leading-normal text-white">
+                              I built a multi-page, comprehensive website showcasing over 20 products and services, which led to a 40% increase in online engagement within the first quarter of launch. By streamlining the user experience through innovative design and intuitive navigation, I reduced bounce rates by 30% and improved average session duration by 25%. Additionally, I optimized the site’s performance using SEO best practices, achieving a top-three ranking for target keywords and increasing organic traffic by 150%, significantly boosting brand visibility.
+                            </p>
+                            <ul class="mt-2 flex flex-wrap" aria-label="Technologies used">
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Multi-page Website</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">UI/UX Design</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Navigation Optimization</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">SEO Best Practices</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Performance Optimization</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Keyword Ranking</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">Organic Traffic Growth</div>
+                              </li>
+                              <li class="mr-1.5 mt-2">
+                                <div class="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">User Engagement</div>
+                              </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+              </ol>
+          </div>
+      </section>
+      
 
         <!-- Skills/Languages -->
         <div class="pt-5 pb-16">
           <h2 class={titleStyles}>Skills/Languages</h2>
-          <p class={subTitleStyles}>A diverse skillset with advanced knowledge spanning across both design and development.</p>
         </div>
 
         <!-- Add Skills/Languages Here -->
-        <ul class="list-none flex flex-wrap justify-center box-border flex-row m-0 p-[0_20px]">
+        <ul class="list-none flex flex-wrap justify-center box-border flex-row m-0 p-[0_20px] mb-8">
           <li class={skillsCardLiStyles}>
             <div class={skillsCardDivStyles}>Python</div>
           </li>
@@ -242,6 +416,13 @@
             <div class={skillsCardDivStyles}>Arduino/RaspberryPI</div>
           </li>
         </ul>
+
+        <!-- Certifications -->
+        <div class="pt-5 pb-16">
+          <h2 class={titleStyles}>Certifications</h2>
+        </div>
+
+
       </div>
   </div>
 </main>
